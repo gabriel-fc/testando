9850ab79dbe8be39d87d13ce4e61268d904a9c6b
gabriel-fc <gfc@ic.ufal.br>
2018-10-02 20:53:50 -0300

    comitando array.js

---
+++ tois/array.js
@@ -0,0 +1,19 @@
+//Array's declaration can be done like these multiple ways:
+
+const array1 = [];
+const array2 = Array();
+const array3 = ["Gabriel", 2, true, 3.14];
+
+//Bidimensional arrays are arrays of arrays:
+
+array1["carros"] = ["uno", 'celta', 'palio'];
+console.log(array1);
+array1.push(21.2);
+console.log(array1);
+array1.pop();
+console.log(array1);
+
+array1.unshift("java")
+console.log(array1);
+array1.push(21.2);
+console.log(array1);
